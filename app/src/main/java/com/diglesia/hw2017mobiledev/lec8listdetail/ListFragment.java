2a43f0a766f2b074ea82135ea98e7d506b0a9aec
dcramalho <delaneyftf@gmail.com>
2017-07-31 20:35:52 -0700

    Added Lab6

---
+++ app/src/main/java/com/diglesia/hw2017mobiledev/lec8listdetail/ListFragment.java
@@ -0,0 +1,46 @@
+package com.diglesia.hw2017mobiledev.lec8listdetail;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.content.Intent;
+
+
+
+
+public class ListFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        final View v = inflater.inflate(R.layout.fragment_list, container, false);
+
+        ListView listView = v.findViewById(R.id.list_view);
+        final String[] strings = new String[]{"hi", "this", "is", "fun"};
+        listView.setAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, strings));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (v.findViewById(R.id.detail_fragment_container) != null) {
+                    DetailFragment fragment = DetailFragment.newInstance(strings[i]);
+                    getFragmentManager().beginTransaction().replace(R.id.detail_fragment_container, fragment).commit();
+                } else {
+                    Intent intent = DetailFragmentActivity.newIntent(getContext(), strings[i]);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+
+
+        return v;
+    }
+
+ }
